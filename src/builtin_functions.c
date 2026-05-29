a2b3e077dc0aa79b859b0e57fb1ce0bf7c91d69b
GilderDragon <zakharov_a_1@mail.ru>
2026-05-29 12:34:07 +0300

    shell_pwd fixed

--- src/builtin_functions.c
+++ src/builtin_functions.c
@@ -5,15 +5,15 @@
 #include "builtin_functions.h"
 
 builtin_func_t builtins[] = {
-    {"help", &shell_help,},
-    {"exit", &shell_exit,},
-    {"cd", &shell_cd,},
-    {"pwd", &shell_pwd,}
+    {"help", &shell_help},
+    {"exit", &shell_exit},
+    {"cd", &shell_cd},
+    {"pwd", &shell_pwd}
 };
 
 int shell_help(char **args) {
-    printf("Упрощенная версия shell\n");
-    printf("Встроенные команды:\n");
+    printf("Simplified shell version\n");
+    printf("Built-in commands:\n");
 
     for (int i = 0; i < count_builtins(); i++) {
         printf("%s\n", builtins[i].name);
@@ -29,24 +29,27 @@ int shell_exit(char **args) {
 int shell_cd(char **args) {
     if (args[1] == NULL) {
         fprintf(stderr, "shell: Invalid argument\n");
+        return EINVAL;
     }
 
     if (chdir(args[1]) != 0) {
         perror("shell");
+        return errno;
     }
 
     return 0;
 }
 
 int shell_pwd(char **args) {
-    char *pwd = getenv("PWD");
-    if (pwd == NULL) {
-        perror("shell");
+    char pwd[1024];
+
+    if (getcwd(pwd, sizeof(pwd)) != NULL) {
+        printf("%s\n", pwd);
+        return 0;
     }
 
-    printf("%s\n", pwd);
-
-    return 0;
+    perror("shell");
+    return errno;
 }
 
 int count_builtins(void) {
