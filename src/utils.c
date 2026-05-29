5273e5cfba4e9cfcb72b2e6f120822b2a18d6611
GilderDragon <zakharov_a_1@mail.ru>
2026-05-29 22:44:26 +0300

    replaced /home with ~

--- src/utils.c
+++ src/utils.c
@@ -9,12 +9,31 @@
 char *get_prompt(void) {
     static char prompt[1024];
     char cwd[512];
+    const char *display_path = cwd;
 
     if (!getcwd(cwd, sizeof(cwd))) {
         strcpy(cwd, "?");
+    } else {
+        const char *home = getenv("HOME");
+        if (!home) {
+            struct passwd *pw = getpwuid(getuid());
+            home = pw ? pw->pw_dir : NULL;
+        }
+
+        if (home) {
+            const size_t home_len = strlen(home);
+
+            if (strncmp(cwd, home, home_len) == 0) {
+                if (cwd[home_len] == '\0' || cwd[home_len] == '/') {
+                    static char tilde_path[512];
+                    snprintf(tilde_path, sizeof(tilde_path), "~%s", cwd + home_len);
+                    display_path = tilde_path;
+                }
+            }
+        }
     }
 
-    snprintf(prompt, sizeof(prompt), "\033[1;32miu7-shell\033[0m:>\033[1;34m%s\033[0m> ", cwd);
+    snprintf(prompt, sizeof(prompt), "\033[1;32miu7-shell\033[0m:\033[1;34m%s\033[0m> ", display_path);
 
     return prompt;
 }
