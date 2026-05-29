92b2569b189d74b7bd625032a44fd296dacb8994
GilderDragon <zakharov_a_1@mail.ru>
2026-05-29 12:57:04 +0300

    runtime module added

---
+++ src/runtime.h
@@ -0,0 +1,12 @@
+#ifndef MINI_SHELL_RUNTIME_H
+#define MINI_SHELL_RUNTIME_H
+
+#define NOT_FOUND (-2)
+
+int execute_command(char **args);
+
+int execute_builtin(char **args);
+
+int execute_external(char **args);
+
+#endif //MINI_SHELL_RUNTIME_H
